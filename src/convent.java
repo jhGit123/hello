7bcb9422a08de0d71c86064cdf295dc232e3ec68
jhGit123 <907965580@qq.com>
2020-08-27 10:36:05 +0800

    commit

---
+++ src/convent.java
@@ -0,0 +1,33 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+/**
+ * 已字符流的方式控制字节流
+ * @author Administrator
+ *
+ */
+public class convent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));){
+			String msg="";
+			while(!msg.equals("exit")) {
+				msg=br.readLine();//逐行读取
+				bw.write(msg);//逐行写出
+				bw.newLine();
+				bw.flush();//强制刷新
+			}
+		}catch (IOException e) {
+			// TODO: handle exception
+			System.out.println("操作有误");
+		}
+		
+	}
+
+}
